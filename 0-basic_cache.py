bd9481e4b0beabb4b7280bc90ef6ba2cdc882918
kimenyu <njorogekimenyu@gmail.com>
2023-10-25 06:06:46 +0000

    0-basic_cache.py

--- 0-basic_cache.py
+++ 0-basic_cache.py
@@ -1,52 +1,53 @@
-#!/usr/bin/python3
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
+#!/usr/bin/env python3
+"""Create a class BasicCache that inherits from
+BaseCaching and is a caching system:
 
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
+You must use self.cache_data - dictionary from
+the parent class BaseCaching
+This caching system doesn’t have limit
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the
+item value for the key key.
+If key or item is None, this method should not do
+anything.
+def get(self, key):
+Must return the value in self.cache_data linked
+to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
+"""
 
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
 
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError("put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError("get must be implemented in your cache class")
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Defines the BasicCache class that inherits from BaseCaching.
+    """_summary_
     """
 
+    def __init__(self):
+        """_summary_
+        """
+        super().__init__()
+
     def put(self, key, item):
-        """assigns the value of the key to the dictionary
+        """_summary_
 
         Args:
-            key (key): dictionary key
-            item (item): dictionary item
+                key (_type_): _description_
+                item (_type_): _description_
         """
-        if key is not None and item is not None:
+        if key is None or item is None:
+            pass
+        else:
             self.cache_data[key] = item
-    
+
     def get(self, key):
-        """ Get an item from the cache. 
-        If key is None or key doesn't exist in the cache, return None.
+        """return the value in self.cache_data linked to key
+
+        Args:
+                key (_type_): _description_
         """
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        return None
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
