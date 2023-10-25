98565ec86c8a2218f0f5660b3be7cfb1416425bd
kimenyu <njorogekimenyu@gmail.com>
2023-10-25 06:41:14 +0000

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
-"""Create a class BasicCache that inherits from
+"""Create a class FIFOCache that inherits from
 BaseCaching and is a caching system:
 
 You must use self.cache_data - dictionary from
 the parent class BaseCaching
-This caching system doesn’t have limit
+You can overload def __init__(self): but don’t
+forget to call the parent init: super().__init__()
 def put(self, key, item):
-Must assign to the dictionary self.cache_data the
-item value for the key key.
-If key or item is None, this method should not do
-anything.
+Must assign to the dictionary self.cache_data
+the item value for the key key.
+If key or item is None, this method should not
+do anything.
+If the number of items in self.cache_data is
+higher that BaseCaching.MAX_ITEMS:
+you must discard the first item put in cache
+(FIFO algorithm)
+you must print DISCARD: with the key discarded and
+following by a new line
 def get(self, key):
-Must return the value in self.cache_data linked
-to key.
+Must return the value in self.cache_data linked to key.
 If key is None or if the key doesn’t exist in
 self.cache_data, return None.
 """
@@ -21,7 +27,6 @@ self.cache_data, return None.
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-from collections import deque
 class FIFOCache(BaseCaching):
     """_summary_
     """
@@ -30,32 +35,30 @@ class FIFOCache(BaseCaching):
         """_summary_
         """
         super().__init__()
-        self.key_order = deque()
-
 
     def put(self, key, item):
         """_summary_
 
         Args:
-            key (_type_): _description_
-            item (_type_): _description_
+                        key (_type_): _description_
+                        item (_type_): _description_
         """
         if key is None or item is None:
-            return
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                first_key = next(iter(self.cache_data.keys()))
+                del self.cache_data[first_key]
+                print("DISCARD: {}". format(first_key))
 
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            removed_key = self.key_order.popleft()
-            self.cache_data.pop(removed_key)
-            print(f"DISCARD: {removed_key}")
-
-        self.cache_data[key] = item
-        self.key_order.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """return the value in self.cache_data linked to key
 
         Args:
-                key (_type_): _description_
+                        key (_type_): _description_
         """
         if key is None or key not in self.cache_data.keys():
             return None
